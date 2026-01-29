0b9fffe1687f7c440404757ed44799610cea4317
Drago1234 <593849302@qq.com>
2026-01-29 20:13:50 +0800

    Merge branch 'master' of https://gitee.com/drago1234/omni-npu into master

--- src/omni_npu/attention/backends/attention.py
+++ src/omni_npu/attention/backends/attention.py
@@ -14,6 +14,7 @@ from typing import List, Optional, Tuple, ClassVar
 import math
 
 import torch
+import torch_npu
 
 from vllm.attention.backends.abstract import (
     AttentionBackend,
@@ -31,7 +32,6 @@ from vllm.v1.attention.backends.utils import (
 )
 from vllm.v1.kv_cache_interface import AttentionSpec
 
-import torch_npu
 
 NZ_DIM = 16
 
