!281 Fix the precision bug in sink-swa-mla * Fix the precision bug in sink-swa-mla

src/omni_npu/attention/backends/mla.py
@@ -708,16 +708,6 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
         layer: AttentionLayer,
     ) -> tuple[torch.Tensor, Optional[torch.Tensor]]:
         assert attn_metadata.decode is not None
-
-        if self.sink_len > 0:
-            query_heads = 1 << (self.num_heads - 1).bit_length()
-            pad_len = query_heads - self.num_heads
-            ql_nope_pad = decode_ql_nope.new_empty((decode_ql_nope.shape[0], pad_len, decode_ql_nope.shape[-1]))
-            decode_ql_nope = torch.cat([decode_ql_nope, ql_nope_pad], dim=1)
-            q_pe_pad = decode_q_pe.new_empty((decode_q_pe.shape[0], pad_len, decode_q_pe.shape[-1]))
-            decode_q_pe = torch.cat([decode_q_pe, q_pe_pad], dim=1)
-        else:
-            query_heads = self.num_heads
             
         # Currently, when use static sink-mla with sliding window,
         # we need to compute sink and normal attention scores separately
@@ -732,7 +722,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                 decode_ql_nope, kv_cache[0], kv_cache[0],
                 query_rope=decode_q_pe,
                 key_rope=kv_cache[1],
-                num_query_heads=query_heads,
+                num_query_heads=self.num_heads,
                 num_key_value_heads=1,
                 input_layout="TND",
                 softmax_scale=self.scale,
@@ -740,6 +730,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                 block_size=128,
                 actual_seq_qlen=attn_metadata.decode.query_cumlens,
                 actual_seq_kvlen=attn_metadata.decode.seq_sink_len,
+                next_tokens=0,
                 return_softmax_lse=True,
             )
 
@@ -749,7 +740,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                     decode_ql_nope, kv_cache[0], kv_cache[0],
                     query_rope=decode_q_pe,
                     key_rope=kv_cache[1],
-                    num_heads=query_heads,
+                    num_heads=self.num_heads,
                     num_key_value_heads=1,
                     input_layout="TND",
                     scale=self.scale,
@@ -760,6 +751,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                     atten_mask=NPUMLAImpl.DECORE_ATTN_MASK,
                     sparse_mode=4,
                     pre_tokens=(self.sliding_window-1),
+                    next_tokens=0,
                     softmax_lse_flag=True,
                 )
             # Compute normal attention scores and LSE without sliding window
@@ -768,7 +760,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                     decode_ql_nope, kv_cache[0], kv_cache[0],
                     query_rope=decode_q_pe,
                     key_rope=kv_cache[1],
-                    num_heads=query_heads,
+                    num_heads=self.num_heads,
                     num_key_value_heads=1,
                     input_layout="TND",
                     scale=self.scale,
@@ -776,6 +768,9 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                     block_size=128,
                     actual_seq_lengths=attn_metadata.decode.query_cumlens,
                     actual_seq_lengths_kv=attn_metadata.decode.seq_lens,
+                    atten_mask=NPUMLAImpl.DECORE_ATTN_MASK,
+                    sparse_mode=3,
+                    next_tokens=0,
                     softmax_lse_flag=True,
                 )
 
@@ -791,7 +786,7 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                 decode_ql_nope, kv_cache[0], kv_cache[0],
                 query_rope=decode_q_pe,
                 key_rope=kv_cache[1],
-                num_heads=query_heads,
+                num_heads=self.num_heads,
                 num_key_value_heads=1,
                 input_layout="TND_NTD",
                 scale=self.scale,
@@ -801,12 +796,9 @@ class NPUMLAImpl(MLACommonBaseImpl[NPUMLAMetadata]):
                 block_size=128,
                 actual_seq_lengths=attn_metadata.decode.query_cumlens,
                 actual_seq_lengths_kv=attn_metadata.decode.seq_lens,
-                atten_mask = NPUMLAImpl.DECORE_ATTN_MASK,
-                sparse_mode = 3
+                atten_mask=NPUMLAImpl.DECORE_ATTN_MASK,
+                sparse_mode=3,
             )[0]
-
-        if self.sink_len > 0:
-            o = o[:self.num_heads]
 
         return o
 