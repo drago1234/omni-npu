5d1cdc51b217386590ff38fe407621f004adc8f6
huting <hudating@hotmail.com>
2026-03-04 08:24:37 +0000

    !335 fix openpanguv2 gqa, support 30a2 * fix openpanguv2 gqa, support 30a2

--- src/omni_npu/vllm_patches/patches/models/pangu_sink_swa_mla/patch_modelconfig.py
+++ src/omni_npu/vllm_patches/patches/models/pangu_sink_swa_mla/patch_modelconfig.py
@@ -36,14 +36,14 @@ class ModelConfigPatch(VLLMPatch):
             "openpangu_mtp",
             "openpangu_v2",
         ):
-            return self.hf_text_config.kv_lora_rank is not None
+            return getattr(self.hf_text_config, "kv_lora_rank", None) is not None
         elif self.hf_text_config.model_type == "eagle":
             # if the model is an EAGLE module, check for the
             # underlying architecture
             return (
                 self.hf_text_config.model.model_type
                 in ("deepseek_v2", "deepseek_v3", "deepseek_v32")
-                and self.hf_text_config.kv_lora_rank is not None
+                and getattr(self.hf_text_config, "kv_lora_rank", None) is not None
             )
         return False
 
