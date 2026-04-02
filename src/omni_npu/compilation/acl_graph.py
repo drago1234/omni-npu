!634 feat (zeron): Fix npu_pangu precision in graph mode.
* Fix npu_pangu accroding to acl_graph update
* Refactor acl_graph.
* Trigger CI again.
* Fix blank spaces.
* Fix npu_pangu precision in graph mode.

src/omni_npu/compilation/acl_graph.py
@@ -453,7 +453,8 @@ class ACLGraphWrapper:
         attn_metadata = {
             key: metadata
             for key, metadata in forward_context.attn_metadata.items()
-            if hasattr(metadata, "decode") and metadata.decode and isinstance(metadata.decode.seq_lens, list)
+            if hasattr(metadata, "decode") and metadata.decode \
+                and hasattr(metadata.decode, "seq_lens") and isinstance(metadata.decode.seq_lens, list)
         }
         graph_params = get_graph_params()
         with torch.npu.stream(update_stream):
@@ -524,7 +525,6 @@ class ACLGraphWrapper:
                     num_kv_heads = param["num_key_value_heads"]
                     input_layout = param["input_layout"]
                     softmax_scale = param["softmax_scale"]
-                    sink_number = param["sink_number"]
                     pre_tokens = param["pre_tokens"]
                     next_tokens = param["next_tokens"]
                     attn_output = param["attn_output"]
@@ -533,6 +533,15 @@ class ACLGraphWrapper:
                     attn_mask = param["atten_mask"]
                     block_tables = param["block_table"]
                     block_size = param["block_size"]
+
+                    sink_kwargs = {}
+                    if "sink_number" in param:
+                        sink_kwargs["sink_number"] = param["sink_number"]
+                    if "key_sink" in param:
+                        sink_kwargs["key_sink"] = param["key_sink"]
+                        sink_kwargs["value_sink"] = param["value_sink"]
+                        sink_kwargs["key_rope_sink"] = param["key_rope_sink"]
+
                     actual_seq_len, actual_seq_len_kv = self._update_fia_params(forward_context, key)
                     if actual_seq_len_kv is None:
                         raise RuntimeError(f"kv length is None. {(forward_context.attn_metadata[key] is None)=}")
@@ -544,7 +553,6 @@ class ACLGraphWrapper:
                         value,
                         query_rope=query_rope,
                         key_rope=key_rope,
-                        sink_number=sink_number,
                         input_layout=input_layout,
                         actual_seq_qlen=actual_seq_len,
                         actual_seq_kvlen=actual_seq_len_kv,
@@ -559,6 +567,7 @@ class ACLGraphWrapper:
                         block_size=block_size,
                         workspace=graph_params.workspaces.get(runtime_shape),
                         out=[attn_output, softmax_lse],
+                        **sink_kwargs, 
                     )
                     torch.npu.graph_task_update_end(update_stream)
                     event.record(update_stream)

src/omni_npu/v1/layers/attention/npu_pangu.py
@@ -10,7 +10,7 @@ from transformers import DeepseekV2Config, DeepseekV3Config
 
 from vllm.model_executor.models.utils import extract_layer_index
 from vllm.distributed import get_tp_group
-from vllm.config import VllmConfig, CacheConfig
+from vllm.config import VllmConfig, CacheConfig, get_current_vllm_config
 from vllm.model_executor.layers.quantization import QuantizationConfig
 from vllm.forward_context import get_forward_context
 from vllm.model_executor.layers.layernorm import RMSNorm
@@ -35,6 +35,10 @@ from omni_npu.layers.attention.npu_sparse_attentions import (
     DSAAttention,
     MomeAttention,
 )
+
+from omni_npu.compilation.utils import capture_multi_fia_sink_graph_size
+
+from vllm.utils.torch_utils import direct_register_custom_op
 
 logger = init_logger(__name__)
 try:
@@ -488,6 +492,11 @@ class NPUPanguSparseAttention(torch.nn.Module):
         self._init_attention_layers()
         self._init_mome_layer()
 
+        compilation_config = get_current_vllm_config().compilation_config
+        if prefix in compilation_config.static_forward_context:
+            raise ValueError(f"Duplicate layer name: {prefix}")
+        compilation_config.static_forward_context[prefix] = self
+
     def _init_MLA_weights(self):
         self.q_a_proj = ReplicatedLinear(
             self.hidden_size,
@@ -775,89 +784,12 @@ class NPUPanguSparseAttention(torch.nn.Module):
         cos: torch.Tensor,
         sin: torch.Tensor,
     ) -> torch.Tensor:
-        attn_metadata = get_forward_context().attn_metadata
-        if isinstance(attn_metadata, dict):
-            mome_metadata = attn_metadata.get(f"{self.prefix}.mome")
-            attn_metadata = attn_metadata.get(f"{self.prefix}.attn")
-        else:
-            mome_metadata = None
-
-        if attn_metadata is None:
-            return self._forward_dummy(
-                hidden_states,
-            )
-        else:
-            num_actual_tokens = attn_metadata.num_actual_tokens
-            has_decode = attn_metadata.num_decodes > 0
-            has_prefill = attn_metadata.num_prefills > 0
-
-            enable_cp = model_extra_config.parall_config.ena_context_parallel and self.is_dsa_layer \
-                        and not has_decode and num_actual_tokens > attn_metadata.num_prefills * self.tp_size * 2
-            if self.tp_size > 1:
-                if not self.all2all_backend == "naive" and not enable_cp:
-                    hidden_states = get_tp_group().all_gather(hidden_states, dim=0)
-
-            if has_decode and has_prefill:
-                prefill_hidden_states, prefill_cos, prefill_sin = self._prepare_phase_inputs(
-                    hidden_states, cos, sin, attn_metadata, 
-                    phase="prefill",
-                )
-                prefill_output = self._forward_prefill(
-                    prefill_hidden_states,
-                    prefill_cos,
-                    prefill_sin,
-                    attn_metadata,
-                    mome_metadata.prefill,
-                )
-
-                decode_hidden_states, decode_cos, decode_sin = self._prepare_phase_inputs(
-                    hidden_states, cos, sin, attn_metadata, 
-                    phase="decode",
-                )
-                decode_output = self._forward_decode(
-                    decode_hidden_states,
-                    decode_cos,
-                    decode_sin,
-                    attn_metadata,
-                    mome_metadata.decode,
-                )
-                
-                self._restore_phase_metadata(attn_metadata)
-
-                hidden_states = torch.cat([decode_output, prefill_output], dim=0)
-            elif attn_metadata.prefill is not None:
-                if enable_cp:
-                    assert self.all2all_backend != "naive", "Context parallel is not supported with naive all2all backend"
-                    return self._forward_prefill_cp(
-                        hidden_states,
-                        cos,
-                        sin,
-                        attn_metadata
-                    )
-                else:
-                    x = hidden_states[:attn_metadata.num_actual_tokens]
-                    attn_output = self._forward_prefill(
-                        x,
-                        cos,
-                        sin,
-                        attn_metadata,
-                        mome_metadata, 
-                    )
-                    hidden_states[:attn_metadata.num_actual_tokens] = attn_output
-            else:
-                hidden_states = self._forward_decode(
-                    hidden_states,
-                    cos,
-                    sin,
-                    attn_metadata,
-                    mome_metadata, 
-                )
-            if self.tp_size > 1:
-                if self.all2all_backend == "naive":
-                    hidden_states = get_tp_group().all_reduce(hidden_states)
-                elif not enable_cp:
-                    hidden_states = get_tp_group().reduce_scatter(hidden_states, dim=0)
-            return hidden_states
+        return torch.ops.vllm.npu_pangu_forward(
+            hidden_states=hidden_states,
+            cos=cos,
+            sin=sin,
+            layer_name=self.prefix,
+        )
 
     def _prepare_phase_inputs(
         self,
@@ -1018,11 +950,12 @@ class NPUPanguSparseAttention(torch.nn.Module):
         sink_k_pe = self.param_sink_k_pe.unsqueeze(1)
 
         query_cumlens = attn_metadata.decode.query_cumlens
+        num_actual_tokens = query_cumlens[-1]
         kwargs = {
-            "query": q_nope[:query_cumlens[-1]],
+            "query": q_nope[:num_actual_tokens],
             "key": kv_cache[0],
             "value": kv_cache[0],
-            "query_rope": q_pe[:query_cumlens[-1]],
+            "query_rope": q_pe[:num_actual_tokens],
             "key_rope": kv_cache[1],
             "num_key_value_heads": 1,
             "input_layout": "TND_NTD",
@@ -1040,22 +973,33 @@ class NPUPanguSparseAttention(torch.nn.Module):
         num_tokens = q_nope.size(0)
         attn_output_shape = [self.num_local_heads, num_tokens, self.kv_lora_rank]
         attn_output = torch.empty(attn_output_shape, device=q_nope.device, dtype=q_nope.dtype)
+        softmax_lse = torch.empty((num_tokens, self.num_local_heads, 1), device=q_nope.device, dtype=torch.float32)
         if self.on_ascend950:
             kwargs.update({
                 "num_heads": self.num_local_heads,
-                "actual_seq_lengths": attn_metadata.decode.query_cumlens,
+                "actual_seq_lengths": query_cumlens,
                 "actual_seq_lengths_kv": attn_metadata.decode.seq_lens,
                 "scale": self.scaling,
             })
-            attn_output[:, :query_cumlens[-1], :] = torch_npu._npu_attention_pioneer(**kwargs)[0]
+            attn_output[:, :num_actual_tokens] = torch_npu._npu_attention_pioneer(**kwargs)[0]
         else:
             kwargs.update({
                 "num_query_heads": self.num_local_heads,
-                "actual_seq_qlen": attn_metadata.decode.query_cumlens,
+                "actual_seq_qlen": query_cumlens,
                 "actual_seq_kvlen": attn_metadata.decode.seq_lens,
                 "softmax_scale": self.scaling,
             })
-            attn_output[:, :query_cumlens[-1], :] = torch.ops.custom.npu_fused_infer_attention_sink(**kwargs)[0]
+            forward_context = get_forward_context()
+            if forward_context.capturing:
+                capture_multi_fia_sink_graph_size(
+                    attn_output=attn_output,
+                    softmax_lse=softmax_lse,
+                    num_tokens=num_tokens,
+                    const_args=kwargs,
+                    layer_name=f"{self.prefix}.attn", 
+                )
+            else:
+                attn_output[:, :num_actual_tokens] = torch.ops.custom.npu_fused_infer_attention_sink(**kwargs)[0]
 
         attn_output = attn_output.view(self.num_local_heads, -1, self.kv_lora_rank) 
         attn_output = (
@@ -1796,3 +1740,115 @@ class NPUPanguSparseAttention(torch.nn.Module):
         hidden_states = self.o_proj(attn_output)
 
         return hidden_states
+
+
+def npu_pangu_forward(
+    hidden_states: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+    layer_name: str,
+) -> torch.Tensor:
+    forward_context = get_forward_context()
+    self = forward_context.no_compile_layers[layer_name]    
+    attn_metadata = get_forward_context().attn_metadata
+    if isinstance(attn_metadata, dict):
+        mome_metadata = attn_metadata.get(f"{self.prefix}.mome")
+        attn_metadata = attn_metadata.get(f"{self.prefix}.attn")
+    else:
+        mome_metadata = None
+
+    if attn_metadata is None:
+        return self._forward_dummy(
+            hidden_states,
+        )
+    else:
+        num_actual_tokens = attn_metadata.num_actual_tokens
+        has_decode = attn_metadata.num_decodes > 0
+        has_prefill = attn_metadata.num_prefills > 0
+
+        enable_cp = model_extra_config.parall_config.ena_context_parallel and self.is_dsa_layer \
+                    and not has_decode and num_actual_tokens > attn_metadata.num_prefills * self.tp_size * 2
+        if self.tp_size > 1:
+            if not self.all2all_backend == "naive" and not enable_cp:
+                hidden_states = get_tp_group().all_gather(hidden_states, dim=0)
+
+        if has_decode and has_prefill:
+            prefill_hidden_states, prefill_cos, prefill_sin = self._prepare_phase_inputs(
+                hidden_states, cos, sin, attn_metadata, 
+                phase="prefill",
+            )
+            prefill_output = self._forward_prefill(
+                prefill_hidden_states,
+                prefill_cos,
+                prefill_sin,
+                attn_metadata,
+                mome_metadata.prefill,
+            )
+
+            decode_hidden_states, decode_cos, decode_sin = self._prepare_phase_inputs(
+                hidden_states, cos, sin, attn_metadata, 
+                phase="decode",
+            )
+            decode_output = self._forward_decode(
+                decode_hidden_states,
+                decode_cos,
+                decode_sin,
+                attn_metadata,
+                mome_metadata.decode,
+            )
+            
+            self._restore_phase_metadata(attn_metadata)
+
+            hidden_states = torch.cat([decode_output, prefill_output], dim=0)
+        elif attn_metadata.prefill is not None:
+            if enable_cp:
+                assert self.all2all_backend != "naive", "Context parallel is not supported with naive all2all backend"
+                return self._forward_prefill_cp(
+                    hidden_states,
+                    cos,
+                    sin,
+                    attn_metadata
+                )
+            else:
+                x = hidden_states[:attn_metadata.num_actual_tokens]
+                attn_output = self._forward_prefill(
+                    x,
+                    cos,
+                    sin,
+                    attn_metadata,
+                    mome_metadata, 
+                )
+                hidden_states[:attn_metadata.num_actual_tokens] = attn_output
+        else:
+            hidden_states = self._forward_decode(
+                hidden_states,
+                cos,
+                sin,
+                attn_metadata,
+                mome_metadata, 
+            )
+        if self.tp_size > 1:
+            if self.all2all_backend == "naive":
+                hidden_states = get_tp_group().all_reduce(hidden_states)
+            elif not enable_cp:
+                hidden_states = get_tp_group().reduce_scatter(hidden_states, dim=0)
+        return hidden_states
+
+
+def npu_pangu_forward_fake(
+    hidden_states: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+    layer_name: str,
+) -> torch.Tensor:
+    return torch.empty_like(hidden_states)
+
+
+direct_register_custom_op(
+    op_name="npu_pangu_forward",
+    op_func=npu_pangu_forward,
+    mutates_args=[],
+    fake_impl=npu_pangu_forward_fake,
+    dispatch_key="PrivateUse1",
+)
+